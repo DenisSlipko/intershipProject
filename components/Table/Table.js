05ec6f301c415d468a3860a022a20b95cc7751f2
Denis Slipko <denisslipko@MacBook-Pro-Denis.local>
2021-11-11 15:15:57 +0200

    add flex layout && implemented interface similar to reference

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -10,21 +10,39 @@ export class Table {
   }
 
   createTable() {
-    this.thead = document.createElement("thead");
+    this.thead = document.createElement("div");
+    this.thead.classList.add("table-header");
     this.tableContainer.append(this.thead);
 
-    this.tbody = document.createElement("tbody");
+    this.tbody = document.createElement("div");
+    this.tbody.classList.add("table-body");
     this.tableContainer.append(this.tbody);
 
     this.createHeader();
   }
 
   createHeader() {
-    const tr = document.createElement("tr");
+    const tr = document.createElement("div");
+    tr.classList.add("table-header-row");
 
     const cells = this.columnsConfig.map((col) => {
-      const cell = document.createElement("th");
+      const cell = document.createElement("div");
+      cell.classList.add("table-header__cell");
+
+      const btnContainer = document.createElement("div");
+      btnContainer.classList.add("btn-container");
+
+      const arrowBtn = document.createElement("span");
+      arrowBtn.classList.add("material-icons");
+      arrowBtn.textContent = "south";
+
+      const menuBtn = document.createElement("span");
+      menuBtn.classList.add("material-icons");
+      menuBtn.textContent = "more_vert";
+
+      btnContainer.append(arrowBtn, menuBtn);
       cell.textContent = col.label;
+      cell.append(btnContainer);
       return cell;
     });
     tr.append(...cells);
@@ -34,9 +52,11 @@ export class Table {
   render(data) {
     const fragment = new DocumentFragment();
     data.forEach((element) => {
-      const rowElement = document.createElement("tr");
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("table-row");
       const cells = this.columnsConfig.map((column) => {
         const cell = document.createElement("td");
+        cell.classList.add("table-row__cell");
         cell.textContent = element[column.key];
 
         return cell;
